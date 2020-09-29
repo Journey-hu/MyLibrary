1da753176d60e467878be0b6dfd7760742b9c45a
huyahui <18103859255@163.com>
2020-09-29 21:30:42 +0800

    1.fix

--- relyontest/src/main/java/com/hu/relyontest/LogUtils.java
+++ relyontest/src/main/java/com/hu/relyontest/LogUtils.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class LogUtils {
 
-    private static final String TAG = "自定义TAG";
+    private static final String TAG = "胡亚辉的输出";
 
     public static void showLog(String msg){
         Log.e(TAG,msg);
