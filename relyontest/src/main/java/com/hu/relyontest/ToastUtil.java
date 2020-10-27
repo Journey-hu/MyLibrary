7702068ed53bf6938796c0c0755b9b000e1813fc
huyahui <18103859255@163.com>
2020-10-27 15:43:06 +0800

    toast工具类

---
+++ relyontest/src/main/java/com/hu/relyontest/ToastUtil.java
@@ -0,0 +1,18 @@
+package com.hu.relyontest;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    @SuppressLint("WrongConstant")
+    public void showToast(Context context, String string){
+        Toast toast=new Toast(context);
+        toast.setGravity(Gravity.BOTTOM,0,0);
+        toast.setDuration(2000);
+        toast.setText(string);
+        toast.show();
+    }
+}
